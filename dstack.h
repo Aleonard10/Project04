e65d8499c38e8a2eac3e12a7d50be68171a71499
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-03-11 17:35:42 -0700

    Update dstack.h

--- dstack.h
+++ dstack.h
@@ -1,1 +1,33 @@
+#ifndef DSTACK_H
+#define DStACK_H
 
+class dstack
+{
+    public:
+        dstack();
+        ~dstack();
+        void push(double value);
+        void pop(double &value);
+        bool empty();
+        void print();
+        int size();
+
+
+
+    private:
+        class Node
+        {
+            public:
+                Node(double value, Node* next)
+                {
+                    m_value = value;
+                    m_next = next;
+                }
+                double m_value;
+                Node* m_next;
+        };
+        Node* m_head;
+        int Size = 0;
+
+};
+#endif
