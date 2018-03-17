169cfe3d8b67e4a2cca1025740afbcfbe9f76ddb
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-03-16 23:59:05 -0700

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,106 +1,99 @@
 #include <iostream>
 #include "dstack.h"
 #include <cmath>
+#include <cctype>
+
 using namespace std;
+
 
 int main()
 {
     dstack stack;
-    //int stackSize = 0;
     double value;
     double newValue;
-    const int MAX = 200;
-    int operatorCount = 0;
-    char Operator[MAX];
-    int size = 0;
+    char Operator;
 
+    char peekChar = cin.peek();
 
-
-    while(cin >> value)
+    while (peekChar != EOF)
     {
-        if (cin.peek() == ' ')
+        if (isspace(peekChar))
         {
             cin.ignore();
-            stack.push(value);
-            size++;
-
         }
-        else if(cin.peek() == '+' || cin.peek() == '-' ||cin.peek() == '/' || cin.peek() == '*' || cin.peek() == '^')
+        else if (isdigit(peekChar))
         {
+            cin >> value;
             stack.push(value);
-            size++;
-            //read the operator pop 2 off stack
-            cin >> Operator[operatorCount];
-            operatorCount++;
+            stack.setSize(1);
+        }
+        else if (ispunct(peekChar))
+        {
+            cin >> Operator;
             double first;
             double second;
-            /* if statements used to check if the stack is empty before removing the top value*/
-            if (stack.empty() == true)
+            // if statements used to check if the stack is empty before removing the top value
+            if (stack.empty() || stack.size() < 2)
             {
                 cerr << "Error: Invalid expression." << endl;
                 exit(1);
             }
             stack.pop(first);
-            size--;
-
-            if (stack.empty() == true)
+            stack.setSize(0);
+            if (stack.empty())
             {
                 cerr << "Error: Invalid expression." << endl;
                 exit(1);
             }
             stack.pop(second);
-            size--;
+            stack.setSize(0);
 
-            if(Operator[operatorCount-1] == '+')
+            if (Operator == '+')
             {
-                newValue = first+second;
-                operatorCount--;
+                newValue = first + second;
                 stack.push(newValue);
-                size++;
+                stack.setSize(1);
             }
-            else if(Operator[operatorCount-1] == '-')
+            else if (Operator == '-')
             {
-                newValue = first-second;
-                operatorCount--;
+                newValue = second - first;
                 stack.push(newValue);
-                size++;
+                stack.setSize(1);
             }
-            else if(Operator[operatorCount-1] == '/')
+            else if (Operator == '/')
             {
                 if (second == 0)
                 {
                     exit(1);
                 }
-                newValue = first/second;
-                operatorCount--;
+                newValue = second / first;
                 stack.push(newValue);
-                size++;
+                stack.setSize(1);
             }
-            else if(Operator[operatorCount-1] == '*')
+            else if (Operator == '*')
             {
-                newValue = first*second;
-                operatorCount--;
+                newValue = first * second;
                 stack.push(newValue);
-                size++;
+                stack.setSize(1);
             }
-            else if(Operator[operatorCount-1] == '^')
+            else if (Operator == '^')
             {
-                newValue = pow(first, second);
-                operatorCount--;
+                newValue = pow(second, first);
                 stack.push(newValue);
-                size++;
-
+                stack.setSize(1);
             }
-
 
         }
+        else
+        {
+            cout << "No condition satisfied." << endl;
+            exit(1);
+        }
+        peekChar = cin.peek();
     }
-    if (size != 1)
-    {
-        exit(1);
-    }
-    else
-    {
-        cout << newValue << endl;
-    }
+    stack.pop(newValue);
+    stack.setSize(0);
+    cout << newValue << endl;
+
+    return 0;
 }
